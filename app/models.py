afe7b174bdc135bd37e34d88533a6de9dfc52a0e
junreycloudstrife <junrey.cloudstrife@gmail.com>
2024-09-25 06:55:38 +0800

    Updated the User model

--- app/models.py
+++ app/models.py
@@ -9,7 +9,6 @@ class Asset(db.Model):
     serial_number = db.Column(db.String(100), nullable=True)   # Serial number for IT Devices
     price = db.Column(db.Float, nullable=False)
     purchase_date = db.Column(db.Date, nullable=True)  # Optional: track purchase dates
-    # purchase_date = db.Column(db.String(20), nullable=True)  # Optional: track purchase dates
     status = db.Column(db.String(50), nullable=False)  # Available, Unusable
     issuances = db.relationship('Issuance', backref='asset', lazy=True)
 
@@ -19,11 +18,9 @@ class User(db.Model):
     name = db.Column(db.String(50), nullable=False)
     role = db.Column(db.String(50), nullable=False)
     position = db.Column(db.String(50), nullable=False)
-    # date_hired = db.Column(db.String(20), nullable=False)
-    # date_resigned = db.Column(db.String(20), nullable=True)
-    date_hired = db.Column(db.Date, nullable=False)
-    is_currently_hired = db.Column(db.Boolean, nullable=False, default=True)
-    date_resigned = db.Column(db.Date, nullable=True)
+    date_started = db.Column(db.Date, nullable=False)
+    has_ended = db.Column(db.Boolean, nullable=False, default=True)
+    date_ended = db.Column(db.Date, nullable=True)
     issuances = db.relationship('Issuance', backref='user', lazy=True)
 
 
