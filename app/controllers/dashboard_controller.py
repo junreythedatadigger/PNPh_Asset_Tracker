0cc3ce266c27ff13b6ec5d1a4de467eed6aba095
junreycloudstrife <junrey.cloudstrife@gmail.com>
2024-09-28 23:12:04 +0800

    Implemented dashboard counters and chart

--- app/controllers/dashboard_controller.py
+++ app/controllers/dashboard_controller.py
@@ -1,25 +1,20 @@
 from flask import Blueprint, render_template
-from ..models import Asset
+from ..models import Asset, User, Issuance
 
 dashboard = Blueprint('dashboard', __name__)
 
 
-# @dashboard.route('/')
-# def home():
-#     return render_template('home_page.html')
-
-
 @dashboard.route('/dashboard')
 def dashboard_view():
-    total_it_devices = Asset.query.filter_by(category='IT Device').count()
-    total_office_furniture = Asset.query.filter_by(category='Office Furniture').count()
-    total_available = Asset.query.filter_by(status='Available').count()
-    total_assigned = Asset.query.filter_by(status='Assigned').count()
-    total_unusable = Asset.query.filter_by(status='Unusable').count()
+
+    total_assets = Asset.query.count()
+    total_users = User.query.count()
+    available_assets = Asset.query.filter_by(status="Available").count()
+    unusable_assets = Asset.query.filter_by(status="Unusable").count()
 
     return render_template('dashboards/dashboard.html',
-                           total_it_devices=total_it_devices,
-                           total_office_furniture=total_office_furniture,
-                           total_available=total_available,
-                           total_assigned=total_assigned,
-                           total_unusable=total_unusable)
+                           total_assets = total_assets,
+                           total_users = total_users,
+                           available_assets = available_assets,
+                           unusable_assets = unusable_assets
+                           )
